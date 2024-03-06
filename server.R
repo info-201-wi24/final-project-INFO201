4cda1e4b6685ae503512ed364a92d5b1847442ef
zerubael <124107675+zerubael@users.noreply.github.com>
2024-03-06 14:14:47 -0800

    Update server.R

--- server.R
+++ server.R
@@ -1,35 +1,16 @@
-library(dplyr)
 library(shiny)
-library(shinythemes)
 library(DT)
 library(leaflet)
 library(tidyverse)
-library(lubridate)
-library(promises)
-library(future)
 library(plotly)
 library(ggplot2)
 library(maps)
 library(mapproj)
 
 cleandata <- read.csv("cleandata.csv")
-cleandata <- cleandata %>%
-  mutate(...1 = NULL,
-         Year = NULL,
-         State.Minimum.Wage = NULL,
-         State.Minimum.Wage.2020.Dollars = NULL,
-         Federal.Minimum.Wage = NULL,
-         Federal.Minimum.Wage.2020.Dollars = NULL,
-         Effective.Minimum.Wage = NULL,
-         CPI.Average = NULL,
-         Department.Of.Labor.Uncleaned.Data = NULL,
-         Department.Of.Labor.Cleaned.Low.Value = NULL,
-         Department.Of.Labor.Cleaned.Low.Value.2020.Dollars = NULL,
-         Department.Of.Labor.Cleaned.High.Value = NULL,
-         Department.Of.Labor.Cleaned.High.Value.2020.Dollars = NULL)
+
 
 server <- function(input, output){
-  data <- read.csv("cleandata.csv", header = TRUE)
   # TODO Make outputs based on the UI inputs here
   
   #  searching the file for the right value to return
